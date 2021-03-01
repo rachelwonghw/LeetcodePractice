42519751d554c90d39ac502b4afe1713e10da2cb
Rachel Wong <hello@rachelwonghw.com>
2021-03-01 04:13:19 -0500

    finished more questions

---
+++ array/169.py
@@ -0,0 +1,6 @@
+import collections
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = collections.Counter(nums)
+        return max(count.keys(), key=count.get)
